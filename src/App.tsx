db157976d8a62a722ff6a51988ff26bf60aeab59
destiny <desphixs@gmail.com>
2026-05-09 03:16:16 +0100

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+    return (
+        <div className='flex justify-center items-center h-screen' >
+            <h1 className='text-3xl font-bold text-blue-500'>Vite + React</h1>
+        </div>
+    )
+}
+
+export default App
